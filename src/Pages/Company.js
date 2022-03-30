43d2da15753ddfeac351f052c67f75828911738a
KolaJosh <ayoolak@ymail.com>
2022-03-30 15:45:43 +0200

    update

--- src/Pages/Company.js
+++ src/Pages/Company.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Careers from "../Components/CompanyComponents/Careers";
 import CompanySlider from "../Components/CompanyComponents/CompanySlider";
 import CSR from "../Components/CompanyComponents/CSR";
@@ -10,6 +11,19 @@ import WhoWeAre from "../Components/CompanyComponents/WhoWeAre";
 function Company() {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>About | Chrisasonfire</title>
+        <link rel="canonical" href="https://www.chrisasonfire.com/about/" />
+        <meta
+          name="description"
+          content="Chrisason Fire is an indigenous Nigerian firm involved in importation, assembling, supply, installation and maintenance of various types of Fire Protection, Fire Fighting, Engineering, Fire Detection Equipment and Systems throughout the nation..."
+        />
+        <meta
+          name="keywords"
+          content="engineering, fire safety, fire Consultng, Startups, About, engine, chrisason info, Chrisasonfire, chrisason"
+        />
+      </Helmet>
       <HeroSectionCompany />
       <WhoWeAre />
       <Mission />
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Form from "../Components/ContactComponent/Form";
 
 function Contact() {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Contact | Chrisasonfire</title>
+        <link rel="canonical" href="https://www.chrisasonfire.com/contact/" />
+        <meta name="description" content="Contact Us at Chriasason Fire" />
+        <meta
+          name="keywords"
+          content="contact, engineering, fire safety, fire Consultng, Startups, About, engine, chrisason info, Chrisasonfire, chrisason, chrisason contact"
+        />
+      </Helmet>
       <Form />
     </>
   );
